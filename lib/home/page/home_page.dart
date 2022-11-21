28d822c5856c0463a36177c3ee891fdeac1e1095
Eldar2021 <eldiiaralmazbekov@gmail.com>
2022-11-21 23:45:41 +0600

    added model ad add product page

--- lib/home/page/home_page.dart
+++ lib/home/page/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../add_product/add_product.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
@@ -8,6 +10,17 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('HomePage'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => AddProductPage(),
+            ),
+          );
+        },
       ),
     );
   }
